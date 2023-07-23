b4b9ea65e66c1249bd009d3551a6528b49b84e67
Cuong Nguyen <ctring23@gmail.com>
2023-07-23 15:51:46 -0400

    Upgrade benchmarking

--- bench.py
+++ bench.py
@@ -1,38 +1,66 @@
+import argparse
+import json
+import os
 import subprocess
+
 import pandas as pd
 
-TXNS = 1000
-TRIALS = 3
+from pprint import pprint
 
-results = []
-for stream in [False, True]:
-    for clients in [1, 2, 4, 8, 16, 32, 64]:
-        for commands in [1, 2, 5, 10, 15, 20, 25, 30]:
+
+def main(args):
+    binary = find_binary()
+    if binary is not None:
+        print(f"Using binary: {binary}")
+    else:
+        print("No binary for the benchmark found")
+        exit(1)
+    results = []
+    for nclient in args.nclients:
+        for ncmd in args.ncmds:
             cmd = [
                 "target/release/client",
-                "--clients", str(clients),
-                "--txns", str(TXNS),
-                "--commands", str(commands),
-                "--simple"
+                "--clients", str(nclient),
+                "--txns", str(args.ntxn),
+                "--commands", str(ncmd),
+                "--json"
             ]
-            if stream:
-                cmd.append("--stream")
-            elapsed_ms = 0
-            for trials in range(TRIALS):
-                output = subprocess.check_output(cmd)
-                elapsed_ms += float(output.strip()) / 1000
-            elapsed_ms /= TRIALS
-            record = {
-                "stream": stream,
-                "clients": clients,
-                "commands": commands,
-                "elapsed_ms": elapsed_ms,
-                "tps": TXNS * clients / (elapsed_ms / 1000),
-                "command_ms": elapsed_ms / (commands * TXNS),
-                "txn_ms": elapsed_ms / TXNS
-            }
+            output = subprocess.check_output(cmd)
+            record = json.loads(output)
+            record.update({"nclient": nclient, "ncmd": ncmd, "nop": 1})
             results.append(record)
-            print(record)
+            pprint(record)
 
-df = pd.DataFrame(results)
-df.to_csv("results.csv", index=False)
+    df = pd.DataFrame(results)
+    df.to_csv(args.output, index=False)
+
+
+def find_binary():
+    if os.path.exists("target/release/client"):
+        return "target/release/client"
+    elif os.path.exists("../target/release/client"):
+        return "../target/release/client"
+    return None
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--nclients",
+        nargs="+",
+        type=int,
+        default=[1, 2, 4, 8, 16],
+        help="Number of clients",
+    )
+    parser.add_argument("--ntxn", default=1000, help="Number of transactions")
+    parser.add_argument(
+        "--ncmds",
+        nargs="+",
+        type=int,
+        default=[2, 3, 6, 11, 16, 21],
+        help="Number of commands per transaction",
+    )
+    parser.add_argument("--output", default="results.csv", help="Output file")
+    args = parser.parse_args()
+
+    main(args)
--- src/client.rs
+++ src/client.rs
@@ -3,20 +3,28 @@ pub mod pb {
 }
 
 use clap::Parser;
-use std::time::Instant;
+use histogram::Histogram;
+use pb::{echo_client::EchoClient, EchoRequest};
+use serde_json::json;
+use std::time::{Duration, Instant};
 use tokio::sync::mpsc;
 use tokio_stream::StreamExt;
 use tonic::{transport::Channel, Request};
 
-use pb::{echo_client::EchoClient, EchoRequest};
-
-async fn unary_streaming_echo(client: &mut EchoClient<Channel>, num: usize) {
+async fn unary_streaming_echo(
+    client: &mut EchoClient<Channel>,
+    num: usize,
+    cmd_latency: &Histogram,
+) {
     for i in 0..num {
         let request = tonic::Request::new(EchoRequest {
             message: format!("msg {:02}", i),
         });
-
+        let cmd_time = Instant::now();
         client.unary_echo(request).await.unwrap();
+        cmd_latency
+            .increment(cmd_time.elapsed().as_nanos() as u64, 1)
+            .unwrap();
     }
 }
 
@@ -64,7 +72,7 @@ struct Cli {
     stream: bool,
 
     #[arg(long, default_value = "false", help = "Simple output")]
-    simple: bool,
+    json: bool,
 }
 
 #[tokio::main]
@@ -72,56 +80,118 @@ struct Cli {
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let cli = Cli::parse();
     let mut tasks = Vec::new();
-    if !cli.simple {
-        println!("Number of clients: {}", cli.clients);
-        println!("Number of transactions: {}", cli.txns);
-        println!("Number of commands per transaction: {}", cli.commands);
-    }
-
     for _ in 0..cli.clients {
         let task = tokio::spawn(async move {
             let mut client = EchoClient::connect("http://[::1]:50051").await.unwrap();
+            let txn_latency = Histogram::builder().build().unwrap();
+            let cmd_latency = Histogram::builder().build().unwrap();
 
-            let start_time = Instant::now();
+            let workload_time = Instant::now();
             for _ in 0..cli.txns {
+                let txn_time = Instant::now();
                 if cli.stream {
                     bidirectional_streaming_echo(&mut client, cli.commands).await;
                 } else {
-                    unary_streaming_echo(&mut client, cli.commands).await;
-                }
+                    unary_streaming_echo(&mut client, cli.commands, &cmd_latency).await;
+                };
+                txn_latency.increment(txn_time.elapsed().as_nanos() as u64, 1);
             }
-            let elapsed = start_time.elapsed().as_micros() as f32;
-            elapsed
+            let elapsed = workload_time.elapsed();
+            BenchmarkResult {
+                tps: cli.txns as f64 / elapsed.as_secs_f64(),
+                txn_latency,
+                cmd_latency,
+                elapsed,
+                committed_txns: cli.txns as u32,
+            }
         });
         tasks.push(task);
     }
 
-    let mut total_elapsed = 0.0;
+    let mut final_result = BenchmarkResult::default();
     for task in tasks {
-        total_elapsed += tokio::join!(task).0.unwrap();
+        let task_result = tokio::join!(task).0.unwrap();
+        final_result.merge(task_result);
     }
 
-    let avg_elapsed = total_elapsed / cli.clients as f32;
-    if cli.simple {
-        println!("{}", avg_elapsed);
-    } else {
-        println!(
-            "Elapsed time per client (avg): {:.2} ms",
-            avg_elapsed / 1000.0
-        );
-        println!(
-            "Latency per txn (avg): {:.2} ms",
-            avg_elapsed / cli.txns as f32 / 1000.0
-        );
-        println!(
-            "Latency per command (avg): {:.2} ms",
-            avg_elapsed / (cli.txns as f32 * cli.commands as f32) / 1000.0
-        );
-        println!(
-            "Txns per second: {:.2}",
-            cli.txns as f32 / (avg_elapsed / 1000000.0) * cli.clients as f32
-        );
-    }
-
+    final_result.show(cli.json);
     Ok(())
+}
+
+pub struct BenchmarkResult {
+    pub tps: f64,
+    pub txn_latency: Histogram,
+    pub cmd_latency: Histogram,
+    pub elapsed: Duration,
+    pub committed_txns: u32,
+}
+
+impl Default for BenchmarkResult {
+    fn default() -> Self {
+        Self {
+            tps: 0.0,
+            txn_latency: Histogram::builder().build().unwrap(),
+            cmd_latency: Histogram::builder().build().unwrap(),
+            elapsed: Duration::default(),
+            committed_txns: 0,
+        }
+    }
+}
+
+impl BenchmarkResult {
+    pub fn merge(&mut self, other: Self) {
+        self.tps += other.tps;
+        self.txn_latency.merge(&other.txn_latency).unwrap();
+        self.cmd_latency.merge(&other.cmd_latency).unwrap();
+        self.elapsed = self.elapsed.max(other.elapsed);
+        self.committed_txns += other.committed_txns;
+    }
+
+    pub fn show(&self, json: bool) {
+        let output = json!({
+            "elapsed": self.elapsed.as_secs_f64(),
+            "committed": self.committed_txns,
+            "tps": self.tps,
+            "txn_lat_min": percentile_ms(&self.txn_latency, 0.0),
+            "txn_lat_p50": percentile_ms(&self.txn_latency, 50.0),
+            "txn_lat_p90": percentile_ms(&self.txn_latency, 90.0),
+            "txn_lat_p99": percentile_ms(&self.txn_latency, 99.0),
+            "txn_lat_max": percentile_ms(&self.txn_latency, 100.0),
+            "cmd_lat_min": percentile_ms(&self.cmd_latency, 0.0),
+            "cmd_lat_p50": percentile_ms(&self.cmd_latency, 50.0),
+            "cmd_lat_p90": percentile_ms(&self.cmd_latency, 90.0),
+            "cmd_lat_p99": percentile_ms(&self.cmd_latency, 99.0),
+            "cmd_lat_max": percentile_ms(&self.cmd_latency, 100.0),
+        });
+
+        if json {
+            println!("{}", serde_json::to_string_pretty(&output).unwrap());
+            return;
+        }
+
+        println!("Elapsed: {:.3} s", output["elapsed"].as_f64().unwrap());
+        println!(
+            "Committed Transactions: {}",
+            output["committed"].as_i64().unwrap()
+        );
+        println!("Throughput: {:.0} txn/s", output["tps"].as_f64().unwrap());
+        println!("Transaction Latency:");
+        println!("\tmin: {:.3} ms", output["txn_lat_min"].as_f64().unwrap());
+        println!("\tp50: {:.3} ms", output["txn_lat_p50"].as_f64().unwrap());
+        println!("\tp90: {:.3} ms", output["txn_lat_p90"].as_f64().unwrap());
+        println!("\tp99: {:.3} ms", output["txn_lat_p99"].as_f64().unwrap());
+        println!("\tmax: {:.3} ms", output["txn_lat_max"].as_f64().unwrap());
+        println!("Command Latency:");
+        println!("\tmin: {:.3} ms", output["cmd_lat_min"].as_f64().unwrap());
+        println!("\tp50: {:.3} ms", output["cmd_lat_p50"].as_f64().unwrap());
+        println!("\tp90: {:.3} ms", output["cmd_lat_p90"].as_f64().unwrap());
+        println!("\tp99: {:.3} ms", output["cmd_lat_p99"].as_f64().unwrap());
+        println!("\tmax: {:.3} ms", output["cmd_lat_max"].as_f64().unwrap());
+    }
+}
+
+fn percentile_ms(hist: &Histogram, p: f64) -> f64 {
+    let bucket = hist.percentile(p).unwrap();
+    let mid = (bucket.high() + bucket.low()) / 2;
+    Duration::from_nanos(mid).as_secs_f64() * 1000.0
 }
